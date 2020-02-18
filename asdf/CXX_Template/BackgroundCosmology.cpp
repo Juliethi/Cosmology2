9fa36f9b717840ec52f6d23b736648bdb01d9292
Julie <skjeggwow@gmail.com>
2020-02-18 18:27:31 +0100

    all files

---
+++ asdf/CXX_Template/BackgroundCosmology.cpp
@@ -0,0 +1,189 @@
+#include "BackgroundCosmology.h"
+
+//====================================================
+// Constructors
+//====================================================
+    
+BackgroundCosmology::BackgroundCosmology(
+    double h, 
+    double OmegaB, 
+    double OmegaCDM, 
+    double OmegaLambda,
+    double Neff, 
+    double TCMB) :
+  h(h),
+  OmegaB(OmegaB),
+  OmegaCDM(OmegaCDM),
+  OmegaLambda(OmegaLambda),
+  Neff(Neff), 
+  TCMB(TCMB)
+{
+  H0 = Constants.H0_over_h*h; //SI units
+  OmegaR = 2*M_PI*M_PI/30 * pow(Constants.k_b*TCMB, 4)/(pow(Constants.hbar,3)*pow(Constants.c,5)) * 8*M_PI*Constants.G/(3*H0*H0);
+}
+
+//====================================================
+// Do all the solving. Compute eta(x)
+//====================================================
+
+// Solve the background
+void BackgroundCosmology::solve(){
+  Utils::StartTiming("Eta");
+  int x_start = -20;
+  int x_end = 5;
+  int n = 1000;
+  Vector x_array = Utils::linspace(x_start, x_end, n);
+
+  // The ODE for deta/dx
+  ODEFunction detadx = [&](double x, const double *eta, double *detadx){
+    detadx[0] = Constants.c/Hp_of_x(x);
+    return GSL_SUCCESS;
+  };
+
+  Vector eta_ic = {0.0};
+  ODESolver ode;
+  ode.solve(detadx, x_array, eta_ic);
+  auto all_data = ode.get_data();
+  Vector eta = Vector(n);
+  
+  for(int i = 0; i < all_data.size(); i++){
+    eta[i] = all_data[i][0];
+  }
+
+  eta_of_x_spline.create(x_array,eta);
+
+  Utils::EndTiming("Eta");
+}
+
+//====================================================
+// Get methods
+//====================================================
+
+double BackgroundCosmology::H_of_x(double x) const{
+  return H0 * sqrt((OmegaB + OmegaCDM)*exp(-3*x) + OmegaR*exp(-4*x) + OmegaLambda);
+}
+
+double BackgroundCosmology::Hp_of_x(double x) const{
+  return H_of_x(x) * exp(x);
+}
+
+double BackgroundCosmology::dHpdx_of_x(double x) const{
+
+  //=============================================================================
+  // TODO: Implement...
+  //=============================================================================
+  //...
+  //...
+
+  return 0.0;
+}
+
+double BackgroundCosmology::ddHpddx_of_x(double x) const{
+
+  //=============================================================================
+  // TODO: Implement...
+  //=============================================================================
+  //...
+  //...
+
+  return 0.0;
+}
+
+double BackgroundCosmology::get_OmegaB(double x) const{ 
+  if(x == 0.0) return OmegaB;
+  double H_over_H0 = H_of_x(x)/H0;
+  return OmegaB/(exp(3*x)*H_over_H0*H_over_H0);
+}
+
+double BackgroundCosmology::get_OmegaR(double x) const{ 
+  if(x == 0.0) return OmegaR;
+  double H_over_H0 = H_of_x(x)/H0;
+  return OmegaR/(exp(4*x)*H_over_H0*H_over_H0);
+}
+
+double BackgroundCosmology::get_OmegaNu(double x) const{ 
+  return 0.0;
+}
+
+double BackgroundCosmology::get_OmegaCDM(double x) const{ 
+  if(x == 0.0) return OmegaCDM;
+  double H_over_H0 = H_of_x(x)/H0;
+  return OmegaCDM/(exp(3*x)*H_over_H0*H_over_H0);
+}
+
+double BackgroundCosmology::get_OmegaLambda(double x) const{ 
+  if(x == 0.0) return OmegaLambda;
+  double H_over_H0 = H_of_x(x)/H0;
+  return OmegaLambda/(H_over_H0*H_over_H0);
+}
+
+double BackgroundCosmology::get_OmegaK(double x) const{ 
+  return 0.0;
+}
+
+double BackgroundCosmology::eta_of_x(double x) const{
+  return eta_of_x_spline(x);
+  //return 0;
+}
+
+double BackgroundCosmology::get_H0() const{ 
+  return H0; 
+}
+
+double BackgroundCosmology::get_h() const{ 
+  return h; 
+}
+
+double BackgroundCosmology::get_Neff() const{ 
+  return Neff; 
+}
+
+double BackgroundCosmology::get_TCMB() const{ 
+  return TCMB; 
+}
+
+//====================================================
+// Print out info about the class
+//====================================================
+void BackgroundCosmology::info() const{ 
+  std::cout << "\n";
+  std::cout << "Info about cosmology class:\n";
+  std::cout << "OmegaB:      " << OmegaB      << "\n";
+  std::cout << "OmegaCDM:    " << OmegaCDM    << "\n";
+  std::cout << "OmegaLambda: " << OmegaLambda << "\n";
+  std::cout << "OmegaK:      " << OmegaK      << "\n";
+  std::cout << "OmegaNu:     " << OmegaNu     << "\n";
+  std::cout << "OmegaR:      " << OmegaR      << "\n";
+  std::cout << "Neff:        " << Neff        << "\n";
+  std::cout << "h:           " << h           << "\n";
+  std::cout << "TCMB:        " << TCMB        << "\n";
+  std::cout << std::endl;
+} 
+
+//====================================================
+// Output some data to file
+//====================================================
+void BackgroundCosmology::output(const std::string filename) const{
+  const double x_min = -16.0;
+  const double x_max =  2.0;
+  const int    n_pts =  1000;
+  
+  Vector x_array = Utils::linspace(x_min, x_max, n_pts);
+
+  std::ofstream fp(filename.c_str());
+  auto print_data = [&] (const double x) {
+    fp << x                  << " ";
+    fp << eta_of_x(x)        << " ";
+    fp << Hp_of_x(x)         << " ";
+    fp << dHpdx_of_x(x)      << " ";
+    fp << get_OmegaB(x)      << " ";
+    fp << get_OmegaCDM(x)    << " ";
+    fp << get_OmegaLambda(x) << " ";
+    fp << get_OmegaR(x)      << " ";
+    fp << get_OmegaNu(x)     << " ";
+    fp << get_OmegaK(x)      << " ";
+    fp <<"\n";
+  };
+  std::for_each(x_array.begin(), x_array.end(), print_data);
+}
+
---
+++ asdf/CXX_Template/milestone1_plots.py
@@ -0,0 +1,109 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import matplotlib
+import seaborn as sns
+matplotlib.rcParams.update({'font.size': 14})
+#sns.set()
+
+values = np.genfromtxt("cosmology.txt")
+n = len(values[:,0])
+
+#x, eta(x), Hp(x), dHp_dx, omegaB, omegaCDM, omegaLambda, OmegaR, OmegaNu, OmegaK"
+Mpc = 3.08567758e22 #Mpc in meters
+x = values[:,0]
+z = 1/np.exp(x) - 1
+
+eta = values[:,1]/Mpc/1000. #Converting eta from m to Mpc
+
+Hp = values[:,2]*Mpc/1000. #Converting Hp to km/s/Mpc
+H = Hp/np.exp(x)
+
+omegaB = values[:,4]
+omegaCDM = values[:,5]
+omegaLambda = values[:,6]
+omegaR = values[:,7]
+
+omegaSum = omegaB + omegaCDM + omegaLambda + omegaR
+omegaMatter = omegaCDM + omegaB
+
+def find_eras():
+    rad_matter_dif = abs(omegaMatter - omegaR)
+    rad_matter_dif = rad_matter_dif[:int(n/2)]
+    rad_matter_eq = np.argmin(rad_matter_dif)
+    #print("x value rad matter eq:", x[rad_matter_eq])
+    
+    matter_dm_dif = abs(omegaMatter - omegaLambda)
+    matter_dm_dif = matter_dm_dif[int(n/2):]
+    matter_dm_eq = np.argmin(matter_dm_dif) + int(n/2)
+    #print("x value matter dm eq:", x[matter_dm_eq])
+
+    return rad_matter_eq, matter_dm_eq
+
+rad_matter_eq, matter_dm_eq = find_eras()
+print(rad_matter_eq, matter_dm_eq)
+
+def plot_omegas():
+    plt.plot(x, omegaLambda, label=r"$\Omega_{\Lambda}$")
+    plt.plot(x, omegaR, label=r"$\Omega_R$")
+    plt.plot(x, (omegaB + omegaCDM), label=r"$\Omega_B + \Omega_{CDM}$")
+    plt.plot(x, omegaSum, "-.", label=r"$\sum \Omega_i$")
+    plt.plot(x,omegaB, "--", label=r"$\Omega_B$")
+    plt.plot(x,omegaCDM, "--",label=r"$\Omega_{CDM}$")
+    plt.axvspan(x[0],x[rad_matter_eq],color="peachpuff")
+    plt.axvspan(x[rad_matter_eq], x[matter_dm_eq], color="powderblue")
+    plt.axvspan(x[matter_dm_eq], x[-1], color="pink")
+    plt.legend()
+    plt.title(r"Time evolution of $\Omega_i$")
+    plt.xlabel("x")
+    plt.ylabel(r"$\Omega$")
+    plt.xlim(x[0],x[-1])
+    plt.show()
+
+plot_omegas()
+
+def plot_H_eta():
+    fig, ax = plt.subplots(2,2,figsize=(14,10))
+    
+    ax[0,0].semilogy(x, H)
+    ax[0,0].set_title("H(x) [km/s/Mpc]")
+    ax[0,0].set_xlabel("x")
+    ax[0,0].set_ylabel("log H")
+    ax[0,0].axvspan(x[0],x[rad_matter_eq],color="peachpuff")
+    ax[0,0].axvspan(x[rad_matter_eq], x[matter_dm_eq], color="powderblue")
+    ax[0,0].axvspan(x[matter_dm_eq], x[-1], color="pink")
+    ax[0,0].set_xlim(x[0],x[-1])
+
+    ax[0,1].loglog(z, H)
+    ax[0,1].set_title("H(z) [km/s/Mpc]")
+    ax[0,1].set_xlabel("log z")
+    ax[0,1].set_ylabel("log H")
+    #ax[0,1].set_xlim(z[0],z[-1])
+    ax[0,1].invert_xaxis()
+    ax[0,1].axvspan(z[0],z[rad_matter_eq],color="peachpuff")
+    ax[0,1].axvspan(z[rad_matter_eq], z[matter_dm_eq], color="powderblue")
+    ax[0,1].axvspan(z[matter_dm_eq], z[-1], color="pink")
+    ax[0,1].set_xlim(z[0],z[-1])
+
+    ax[1,0].semilogy(x, Hp)
+    ax[1,0].set_title(r"$\mathcal{H}(x)$ [km/s/Mpc]")
+    ax[1,0].set_xlabel("x")
+    ax[1,0].set_ylabel(r"$\log \mathcal{H}$")
+    ax[1,0].axvspan(x[0],x[rad_matter_eq],color="peachpuff")
+    ax[1,0].axvspan(x[rad_matter_eq], x[matter_dm_eq], color="powderblue")
+    ax[1,0].axvspan(x[matter_dm_eq], x[-1], color="pink")
+    ax[1,0].set_xlim(x[0],x[-1])
+
+
+    ax[1,1].semilogy(x, eta)
+    ax[1,1].set_title(r"$\eta(x)$ [Gpc]")
+    ax[1,1].set_xlabel("x")
+    ax[1,1].set_ylabel(r"$\log \eta$")
+    ax[1,1].axvspan(x[0],x[rad_matter_eq],color="peachpuff")
+    ax[1,1].axvspan(x[rad_matter_eq], x[matter_dm_eq], color="powderblue")
+    ax[1,1].axvspan(x[matter_dm_eq], x[-1], color="pink")
+    ax[1,1].set_xlim(x[0],x[-1])
+
+    fig.tight_layout()
+    plt.show()
+
+plot_H_eta()
